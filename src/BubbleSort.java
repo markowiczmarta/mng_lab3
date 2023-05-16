bffc9c7cb8e515706e650c1bf7321158b2b39d4c
martamarkowicz <markowiczmarta5@gmail.com>
2023-05-16 11:15:04 +0200

    Initial commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,21 @@
+public class BubbleSort {
+    static void bubblesort(int tab[]) {
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i< tab.length; i++){
+            System.out.print(tab[i]+" ");
+        }
+    }
+
+    public static void main(String[] args){
+        int[] tab = {1, 4, 2137, 420, 2, 68, 546423, 3, 4, 5};
+        bubblesort(tab);
+    }
+}
